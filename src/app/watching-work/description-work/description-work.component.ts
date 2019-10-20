5841065d57fbdcc8dc4157620ad649b955fae090
Shestakova Sofya <soneshya@mail.ru>
2019-10-20 14:16:06 +0300

    Fixed slidebar Fixed watch-works page

---
+++ src/app/watching-work/description-work/description-work.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-description-work',
+  templateUrl: './description-work.component.html',
+  styleUrls: ['./description-work.component.scss']
+})
+export class DescriptionWorkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
