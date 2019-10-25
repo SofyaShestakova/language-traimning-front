0dc5ac7fa16fdcab50dc47343785e2bad36bf5a9
Pavel Timofeev <timos500@mail.ru>
2019-10-26 01:56:19 +0300

    add ability to add themes

--- src/app/forum/forum.component.ts
+++ src/app/forum/forum.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Theme} from '../shared/interfaces';
 import {ThemeService} from '../shared/services/theme.service';
 import {ActivatedRoute} from '@angular/router';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-forum',
@@ -17,8 +18,23 @@ export class ForumComponent implements OnInit {
   }
 
   themes: Theme[] = this.themeService.themes;
+  showForm = false;
+  form: FormGroup;
 
   ngOnInit() {
+    this.form = new FormGroup({
+      themeTitle: new FormControl(null, Validators.required)
+    });
+  }
+
+  addTheme() {
+    this.themes.push( {
+      id: Math.round(Math.random()*100000000),
+      title: this.form.value.themeTitle,
+      messages : []
+    },);
+    this.form.reset();
+    this.showForm = false;
   }
 
 }
